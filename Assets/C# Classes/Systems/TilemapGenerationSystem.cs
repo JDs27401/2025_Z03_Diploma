9e33972f20e9cf2c4f303a120ac5291cc82e4244
JDs27401 <s27401@pjwstk.edu.pl>
2026-01-09 23:04:52 +0100

    First try on tilemap generation

---
+++ Assets/C# Classes/Systems/TilemapGenerationSystem.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using C__Classes.Systems;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapGenerationSystem : MonoBehaviour
+{
+    [SerializeField] 
+    private string hash;
+    [SerializeField]
+    private Tilemap tilemap;
+    [SerializeField]
+    private TileBase waterTile;
+    [SerializeField]
+    private TileBase groundTile;
+    [SerializeField]
+    private TileBase highValueTile;
+
+    [SerializeField] 
+    private int mapSize = 1024;
+    [SerializeField]
+    private int chunkSize = 128;
+    
+
+    private void Start()
+    {
+        RunProceduralGeneration();
+    }
+    
+    private void RunProceduralGeneration()
+    {
+        GenerateTilemap();
+    }
+
+    private void GenerateTilemap()
+    {
+        float[,] tileValues = MapGenerationSystem.GenerateMap(mapSize, chunkSize, hash);
+
+        for (int y = 0; y < tileValues.GetLength(1); y++)
+        {
+            for (int x = 0; x < tileValues.GetLength(0); x++)
+            {
+                var position = tilemap.WorldToCell(new Vector3(x, 0, y));
+                tilemap.SetTile(position, SelectTile(tileValues[x,y]));
+            }    
+        }
+    }
+
+    private TileBase SelectTile(float value)
+    {
+        if (value < 0.2f)
+        {
+            return waterTile;
+        }
+        if (value < 0.9f)
+        {
+            return groundTile;
+        }
+        return highValueTile;
+        
+        /*return value switch
+        {
+            < 0.2f => waterTile,
+            < 0.9f => groundTile,
+            _      => highValueTile
+        };*/
+    }
+}
