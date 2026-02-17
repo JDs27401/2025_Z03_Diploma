98dc60fff3ec8d0e81ad3cc2e997444657250419
JDs27401 <s27401@pjwstk.edu.pl>
2026-02-17 23:44:09 +0100

    Working trap implementation
    Only issue is with damage being applied twice to NPCs if trigger is not destroyed immediately. To be looked into further.

---
+++ Assets/C# Classes/ExplodingTrap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace C__Classes
+{
+    public class ExplodingTrap : Actor
+    {
+        [SerializeField] private Collider2D TrapTrigger;
+        [SerializeField] private Collider2D DamageTrigger;
+
+        private new void Start()
+        {
+            if (ReferenceEquals(TrapTrigger, null))
+            {
+                return;
+            }
+            DamageTrigger.enabled = false;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            tag = "trap";
+            DamageTrigger.enabled = true;
+            
+            print("boom");
+            Destroy(DamageTrigger);
+            Destroy(gameObject, waitUntilDestroyed);
+        }
+    }
+}
