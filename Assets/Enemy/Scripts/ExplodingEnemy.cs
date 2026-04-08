d56f25179f88df36bfd55b22806f04dca524ffac
JDs27401 <s27401@pjwstk.edu.pl>
2026-04-08 18:34:53 +0200

    Refactored Exploding Enemie's player tracking to a new Component - PlayerTrackingComponent

--- Assets/Enemy/Scripts/ExplodingEnemy.cs
+++ Assets/Enemy/Scripts/ExplodingEnemy.cs
@@ -5,12 +5,6 @@ namespace Enemy.Scripts
 {
     public class ExplodingEnemy : NpcBase
     {
-        protected override void Start()
-        {
-            base.Start();
-            Aggravate(GameObject.FindWithTag("player").transform);
-        }
-
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (!other.CompareTag("player"))
